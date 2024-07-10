12ece512ba4e59b1e5157469bb5d0fe709c0e959
Yukimokki <yana.domnina@gmail.com>
2024-07-10 05:17:15 +0300

    1st develop commit, no job done

---
+++ src/parser.py
@@ -0,0 +1,33 @@
+import requests
+from abc import ABC, abstractmethod
+
+class Parser(ABC):
+
+    @abstractmethod
+    def load_vacancies(self,keyword):
+        pass
+
+
+class HH(Parser):
+    """
+    Класс для работы с API Headhunter
+    родиетльский класс - Parser
+    """
+
+    def __init__(self, keyword):
+        self.url = "https://api.hh.ru/vacancies"
+        self.headers = {'User-Agent':'HH-User-Agent'}
+        self.params = {'text': keyword, 'page':0, 'per_page':50}
+        self.vacancies = []
+
+
+    def load_vacancies(self, *args, **kwargs):
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers = self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
+
+hh = HH("backend-Python")
+hh.load_vacancies()
+print(hh.vacancies)
